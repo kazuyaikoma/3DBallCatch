1050b906a5d692036bdf1ce04e34dbaf3118ba41
nixiesquid <audu817@gmail.com>
2019-09-10 12:34:43 +0900

    implement random SpawnBall

--- Assets/Scripts/BallFactoryScript.cs
+++ Assets/Scripts/BallFactoryScript.cs
@@ -9,13 +9,12 @@ public class BallFactoryScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(ball, transform.position, transform.rotation);
-        //InvokeRepeating()
+        InvokeRepeating("SpawnBall", 0f, 1f);
     }
 
     void SpawnBall()
     {
-
+        Instantiate(ball, new Vector3(Random.Range(-5f, 5f), transform.position.y, transform.position.z), transform.rotation);
     }
 
     // Update is called once per frame
